899d07fad6e7ca9d375b891dd4712230f978d2b0
John Doe <virusavuk@gmail.com>
2020-08-13 16:13:16 +0300

    init

--- public/script.js
+++
@@ -1,8 +0,0 @@
-$('.slider').owlCarousel({
-               items: 1,
-               singleItem:true,
-               nav: true,
-               dots: false,
-               loop: true,
-               autoPlay: 3000
-           });
